64886189a6c63e8b55b16721a4a119a6490a5663
ericwstevenson <42499776+ericwstevenson@users.noreply.github.com>
2020-03-18 12:59:53 -0500

    Add files via upload

--- model/database.php
+++ model/database.php
@@ -1,11 +1,11 @@
 <?php
-    $dsn = 'mysql:host=localhost;dbname=zippyusedautos';
-    $username = 'root';
-    //$password = 'pa55word';
-
+    $dsn = 'p9ua05r2r4j5isgx';
+    $username = 'tdmw4pluehmdc6ll';
+    $password = 'i543o4gm51d5olk2';
+	
     try {
         $db = new PDO($dsn, $username);
-        //$db = new PDO($dsn, $username, $password);
+        $db = new PDO($dsn, $username, $password);
     } catch (PDOException $e) {
         $error_message = $e->getMessage();
         include('../errors/database_error.php');
